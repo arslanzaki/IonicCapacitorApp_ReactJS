beb92158962513e34c4f156b4961c770c15e0664
arslanzaki <arslanzaki223@gmail.com>
2023-08-11 16:52:47 +0500

    Using Capacitor Plugins

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,20 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
 
 const Tab1: React.FC = () => {
 
+    const [image, setImage] = useState<any>(null);
+
+    const takePicture = async () => {
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: false,
+            resultType: CameraResultType.Base64
+        })
+        const img = `data:image/jpeg;base64, ${image.base64String}`;
+        setImage(img)
+    }
     return (
         <IonPage>
             <IonHeader>
@@ -14,7 +26,8 @@ const Tab1: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                UI goes here...
+                <IonButton expand="block" onClick={takePicture}>Take Picture</IonButton>
+                <img src={image} />
             </IonContent>
         </IonPage>
     );
